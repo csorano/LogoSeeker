1f81c3004da305b8788ae794003d8e4a66f087d6
Legrand <dlegrand@elv.telecom-lille.fr>
2017-11-07 17:51:10 +0100

    Interface Application Library/AP
    Le boutton Library est fonctionnel.
    Les photos prise avec l'appareil photo apparessent dans la galerie.
    La miniature de la photo prise ou sélectionné n'est plus rogné.

--- LogoSeeker/app/src/main/java/imt/logoseeker/MainActivity.java
+++ LogoSeeker/app/src/main/java/imt/logoseeker/MainActivity.java
@@ -1,19 +1,24 @@
 package imt.logoseeker;
 
 import android.content.Intent;
+import android.database.Cursor;
 import android.graphics.Bitmap;
 import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Environment;
 import android.provider.MediaStore;
+import android.provider.Settings;
 import android.support.v4.content.FileProvider;
 import android.support.v4.util.LogWriter;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.Button;
+import android.widget.Gallery;
 import android.widget.ImageView;
+import android.widget.Toast;
 
 import java.io.Console;
 import java.io.File;
@@ -25,21 +30,31 @@ import java.util.logging.Logger;
 public class MainActivity extends AppCompatActivity {
 
     final int REQUEST_TAKE_PHOTO = 1;
+    final int RESULT_LOAD_IMAGE=2;
     String mCurrentPhotoPath;
 
+    //Action du bouton capture
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Button button = (Button)findViewById(R.id.b_capture);
-        button.setOnClickListener(new View.OnClickListener() {
+        Button buttonCap = (Button)findViewById(R.id.b_capture);
+        buttonCap.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 dispatchTakePictureIntent();
             }
         });
+
+        Button buttonLib = (Button)findViewById(R.id.b_Library);
+        buttonLib.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                dispatchViewLibrary();
+            }
+        });
     }
 
+    //Lancement de l'appareil photo
     private void dispatchTakePictureIntent() {
         Intent takePictureIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
         // Ensure that there's a camera activity to handle the intent
@@ -53,9 +68,7 @@ public class MainActivity extends AppCompatActivity {
             // Continue only if the File was successfully created
             if (photoFile != null) {
                 try {
-                    Uri photoURI = FileProvider.getUriForFile(this,
-                            "imt.logoseeker.fileprovider",
-                            photoFile);
+                    Uri photoURI = FileProvider.getUriForFile(this, "imt.logoseeker.fileprovider", photoFile);
                     takePictureIntent.putExtra(MediaStore.EXTRA_OUTPUT, photoURI);
                     startActivityForResult(takePictureIntent, REQUEST_TAKE_PHOTO);
                 }
@@ -67,24 +80,14 @@ public class MainActivity extends AppCompatActivity {
         }
     }
 
-    @Override
-    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-
-        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
-            ImageView img = (ImageView) findViewById(R.id.v_picture);
-
-            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath,new BitmapFactory.Options());
-            img.setImageBitmap(imageBitmap);
-
-            galleryAddPic();
-        }
-    }
-
+    //Nommage de la photo
     private File createImageFile() throws IOException {
         // Create an image file name
         String timeStamp = new SimpleDateFormat("yyyyMMdd_HHmmss").format(new Date());
         String imageFileName = "LOGO_" + timeStamp + "_";
-        File storageDir = getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+        final String appDirectoryName = "LogoSeeker";
+        final File storageDir = new File(Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_PICTURES), appDirectoryName);
+        storageDir.mkdirs();
         File image = File.createTempFile(
                 imageFileName,  /* prefix */
                 ".jpg",         /* suffix */
@@ -96,6 +99,44 @@ public class MainActivity extends AppCompatActivity {
         return image;
     }
 
+
+    //Visionneur de photo
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        if (requestCode == REQUEST_TAKE_PHOTO && resultCode == RESULT_OK) {
+            ImageView img = (ImageView) findViewById(R.id.v_picture);
+
+            Bitmap imageBitmap = BitmapFactory.decodeFile(mCurrentPhotoPath,new BitmapFactory.Options());
+            img.setImageBitmap(imageBitmap);
+
+
+            Toast.makeText(MainActivity.this, "Photo Captured", Toast.LENGTH_SHORT).show();
+
+           galleryAddPic();
+        }
+
+        if (requestCode == RESULT_LOAD_IMAGE && resultCode == RESULT_OK){
+
+            Uri selectedImage = data.getData();
+            String[] filePathColumn = { MediaStore.Images.Media.DATA };
+
+            Cursor cursor = getContentResolver().query(selectedImage,
+                    filePathColumn, null, null, null);
+            cursor.moveToFirst();
+
+            int columnIndex = cursor.getColumnIndex(filePathColumn[0]);
+            String picturePath = cursor.getString(columnIndex);
+            cursor.close();
+
+            ImageView img = (ImageView) findViewById(R.id.v_picture);
+            img.setImageBitmap(BitmapFactory.decodeFile(picturePath));
+
+        }
+    }
+
+
+
     private void galleryAddPic() {
         Intent mediaScanIntent = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
         File f = new File(mCurrentPhotoPath);
@@ -104,4 +145,14 @@ public class MainActivity extends AppCompatActivity {
         this.sendBroadcast(mediaScanIntent);
     }
 
+    private void dispatchViewLibrary(){
+
+        Intent i = new Intent(
+                Intent.ACTION_PICK,
+                android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+
+        startActivityForResult(i, RESULT_LOAD_IMAGE);
+    }
+
 }
+
